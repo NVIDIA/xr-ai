a312be5d4f9722f1802e5cee146db7d4889f7cda
Brent Longstaff <blongstaff@nvidia.com>
2026-09-18 14:42:03 -0700

    Fix native stub file-transfer connection checks
    Signed-off-by: Brent Longstaff <blongstaff@nvidia.com>

--- client-samples/native/StreamKit/src/Backends/LiveKit/LiveKitBackend.cpp
+++ client-samples/native/StreamKit/src/Backends/LiveKit/LiveKitBackend.cpp
@@ -614,7 +614,11 @@ FileTransferInfo LiveKitBackend::SendBytes(
     std::uint64_t generation;
     {
         std::scoped_lock lock(teardown_mutex_);
-        if (!is_connected_.load() || !room_) {
+        if (!is_connected_.load()
+#if STREAMKIT_HAVE_LIVEKIT
+            || !room_
+#endif
+        ) {
             throw NotConnectedError{};
         }
         active_room = room_;
@@ -664,7 +668,11 @@ FileTransferInfo LiveKitBackend::SendFile(
     std::uint64_t generation;
     {
         std::scoped_lock lock(teardown_mutex_);
-        if (!is_connected_.load() || !room_) {
+        if (!is_connected_.load()
+#if STREAMKIT_HAVE_LIVEKIT
+            || !room_
+#endif
+        ) {
             throw NotConnectedError{};
         }
         active_room = room_;
